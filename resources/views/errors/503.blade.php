bfa549c7e293b2743226169e822abe8006aaa8b2
吕海涛 <git@lvht.net>
2016-02-28 21:04:50 +0800

    init

---
+++ resources/views/errors/503.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Be right back.</title>
+    </head>
+    <body>
+        <div class="container">
+            <div class="content">
+                <div class="title">Be right back.</div>
+            </div>
+        </div>
+    </body>
+</html>
